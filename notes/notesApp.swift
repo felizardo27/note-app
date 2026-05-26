62b29be31a5b2b19c1d236f8ea45fcc15a34e45c
felizardo27 <jp.felizardo27@gmail.com>
2026-05-26 11:31:54 -0300

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by João Pedro on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
